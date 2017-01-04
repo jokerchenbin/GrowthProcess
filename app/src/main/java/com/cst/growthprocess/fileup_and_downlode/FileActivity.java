9590add76bfd5a5aafe241614eef8579ec4dac2a
陈彬 <771214723@qq.com>
2017-01-04 11:27:45 +0800

    新增 普通文件的下载

--- app/src/main/java/com/cst/growthprocess/fileup_and_downlode/FileActivity.java
+++ app/src/main/java/com/cst/growthprocess/fileup_and_downlode/FileActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.cst.growthprocess.R;
@@ -17,6 +18,7 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.BinaryHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -30,11 +32,18 @@ import cz.msebera.android.httpclient.Header;
 
 public class FileActivity extends AppCompatActivity {
 
-    @InjectView(R.id.download)
-    Button mDownload;
+
+    @InjectView(R.id.download_image_file)
+    Button mDownloadImageFile;
+
+    @InjectView(R.id.download_common_file)
+    Button mDownloadCommonFile;
 
     @InjectView(R.id.upload)
     Button mUpload;
+
+    @InjectView(R.id.activity_file)
+    RelativeLayout mActivityFile;
 
     private Context mContext;
 
@@ -46,26 +55,84 @@ public class FileActivity extends AppCompatActivity {
         mContext = this;
     }
 
-    @OnClick({R.id.download, R.id.upload})
+
+    @OnClick({R.id.download_image_file, R.id.download_common_file, R.id.upload})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.download:
-                downLoadFile();
+            case R.id.download_image_file:
+                downLoadImageFile();
+                break;
+            case R.id.download_common_file:
+                downCommonFile();
                 break;
             case R.id.upload:
-                upLoadFile();
+                upLoadImageAndCommonFile();
                 break;
         }
     }
 
+
     /**
-     * 上传文件
+     * 下载普通文件
      */
-    private void upLoadFile() {
+    private void downCommonFile() {
+
+        String url = "http://172.19.14.216/cb/license.txt";
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        // 指定文件类型
+        String[] allowedContentTypes = new String[]{"text/plain"};
+        // 获取二进制数据如图片和其他文件
+        client.get(url, new BinaryHttpResponseHandler(allowedContentTypes) {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers,
+                    byte[] binaryData) {
+                String tempPath = Environment.getExternalStorageDirectory()
+                        .getPath() + "/0_chenbin";
+                // TODO Auto-generated method stub
+                // 下载成功后需要做的工作
+                Log.e("binaryData:", "共下载了：" + binaryData.length);
+                byte2File(binaryData, tempPath, "license.txt");
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers,
+                    byte[] binaryData, Throwable error) {
+                // TODO Auto-generated method stub
+                Toast.makeText(mContext, "下载失败", Toast.LENGTH_LONG).show();
+                Log.e("binaryData", error.getMessage() + " / ");
+            }
+
+            @Override
+            public void onProgress(long bytesWritten, long totalSize) {
+                super.onProgress(bytesWritten, totalSize);
+                int count = (int) ((bytesWritten * 1.0 / totalSize) * 100);
+                // 下载进度显示
+                Log.e("binaryData", bytesWritten + " / " + totalSize);
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // TODO Auto-generated method stub
+                super.onRetry(retryNo);
+                // 返回重试次数
+            }
+
+        });
+
+    }
+
+    /**
+     * 上传普通和图片文件文件
+     */
+    private void upLoadImageAndCommonFile() {
 
         String url = "http://172.19.14.216/cb/upload_file.php";
         String path = Environment.getExternalStorageDirectory().getPath()
-                + "/0_chenbin/uncaughtException"+File.separator+"chenbi.txt";
+                + "/0_chenbin/uncaughtException" + File.separator + "chenbi.txt";
         File file = new File(path);
         if (file.exists() && file.length() > 0) {
             AsyncHttpClient client = new AsyncHttpClient();
@@ -116,14 +183,14 @@ public class FileActivity extends AppCompatActivity {
     /**
      * 下载图片文件
      */
-    private void downLoadFile() {
+    private void downLoadImageFile() {
 
-        String url = "http://172.19.14.216/cb/upload/chenbi.txt";
+        String url = "http://172.19.14.216/cb/upload/abc.png";
 
         AsyncHttpClient client = new AsyncHttpClient();
 
         // 指定文件类型
-        String[] allowedContentTypes = new String[]{"text/plain"};
+        String[] allowedContentTypes = new String[]{"image/png", "image/jpeg"};
         // 获取二进制数据如图片和其他文件
         client.get(url, new BinaryHttpResponseHandler(allowedContentTypes) {
 
@@ -131,7 +198,7 @@ public class FileActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Header[] headers,
                     byte[] binaryData) {
                 String tempPath = Environment.getExternalStorageDirectory()
-                        .getPath() + "/0_chenbin/temp.txt";
+                        .getPath() + "/0_chenbin/temp.png";
                 // TODO Auto-generated method stub
                 // 下载成功后需要做的工作
                 Log.e("binaryData:", "共下载了：" + binaryData.length);
@@ -194,4 +261,45 @@ public class FileActivity extends AppCompatActivity {
         });
 
     }
+
+
+    /**
+     * byte数字转文件
+     */
+    public void byte2File(byte[] buf, String filePath, String fileName) {
+        BufferedOutputStream bos = null;
+        FileOutputStream fos = null;
+        File file = null;
+        try {
+            File dir = new File(filePath);
+            if (!dir.exists() && dir.isDirectory()) {
+                dir.mkdirs();
+            }
+            file = new File(filePath + File.separator + fileName);
+            fos = new FileOutputStream(file);
+            bos = new BufferedOutputStream(fos);
+            bos.write(buf);
+            Toast.makeText(FileActivity.this, "下载成功\n" + filePath,
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 }
